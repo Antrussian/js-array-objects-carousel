f6640139be6426db843a9fc99b9c3c1cae8f87b5
Antrussian <antonino.russo.agent@gmail.com>
2023-12-05 15:54:28 +0100

    Fine Esercizio

--- js/script.js
+++ js/script.js
@@ -21,54 +21,62 @@ Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 
 
 */
-
-
 const images = [
     {
         image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
+        title: "Marvel's Spiderman Miles Morale",
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
+    },
+    {
         image: 'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
+    },
+    {
         image: 'img/03.webp',
         title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
+        text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+    },
+    {
         image: 'img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
+    },
+    {
         image: 'img/05.webp',
         title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
+        text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+    },
 ];
 
-
- 
-const wrapperElement = document.getElementById('carousel-container'); 
+const wrapperElement = document.getElementById('carousel-container');
 const imageContainer = document.querySelector('.my-carousel-images');
- 
- images.forEach(image => {
-   const newImageElement = document.createElement('img');
-   const newTitleElement = document.createElement ('p');
-   const newTextElement = document.createElement ('p');
+let currentIndex = 0;
 
+function updateCarousel() {
+    const currentImage = images[currentIndex];
 
-   newImageElement.src = image.image; // imposta l'attributo src con l'URL dell'immagine
-   newImageElement.alt = image.title; // imposta l'attributo alt con il titolo dell'immagine
-   newTitleElement.textContent = image.title;
-   newTextElement.textContent = image.text;
+    // Aggiorna l'immagine principale
+    imageContainer.innerHTML = '';
+    const newImageElement = document.createElement('img');
 
+    newImageElement.src = currentImage.image;
+    newImageElement.alt = currentImage.title;
 
+    imageContainer.appendChild(newImageElement);
+}
 
+function ArrowClick(direction) {
+    if (direction === 'up') {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+    } else {
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+    updateCarousel();
+}
 
-   imageContainer.appendChild(newImageElement); // aggiungi l'immagine al container
-   imageContainer.appendChild(newTitleElement);
-   imageContainer.appendChild(newTextElement);
- });
- 
- 
+// Event listeners per le frecce
+document.getElementById('arrow-up').addEventListener('click', () => ArrowClick('up'));
+document.getElementById('arrow-down').addEventListener('click', () => ArrowClick('down'));
+
+updateCarousel();
